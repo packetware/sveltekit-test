ef1da4fb252e601ed5b4ff827eaf86c74f1d9ac3
ultrasive <github@aidanperry.net>
2024-03-04 13:57:13 -0600

    dynamic useAdapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,24 @@
 import adapterAuto from '@sveltejs/adapter-auto';
 import adapterNode from '@sveltejs/adapter-node';
 
+export function useAdapter(environment) {
+	switch (environment) {
+		case 'node': {
+			return adapterNode();
+		}
+		default: {
+			return adapterAuto();
+		}
+	}
+}
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: switch (process.env.PLATFORM) {
-		      case 'node': {
-		        return adapterNode();
-		      }
-		      default: {
-		        return adapterAuto();
-		}
+		adapter: useAdapter(process.env.PLATFORM),
 	}
 };
 
